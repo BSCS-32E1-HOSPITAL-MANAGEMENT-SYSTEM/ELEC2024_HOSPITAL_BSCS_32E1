8d0db49e43aa8ef82618bbbd6c72b55a13c96a39
Agutaya_Peter_Dave_Andrew_BSCS32E1 <agutayaandrew@gmail.com>
2024-06-15 18:05:17 +0800

    Update DeleteAnnouncementOnExpire

--- Models/DeleteAnnouncementOnExpire.cs
+++ Models/DeleteAnnouncementOnExpire.cs
@@ -6,7 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Services.Description;
 
-namespace Hospital_Management_System.Models
+namespace BSCS_32E1_HOSPITAL_MANAGEMENT_SYSTEM.Models
 {
     public static class DeleteAnnouncementOnExpire
     {

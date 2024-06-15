0c36386ab842f06aefed3c91b0ad0382e88d5830
Borillo-Miguel-BSCS32E1 <miguelborillo2002@gmail.com>
2024-06-15 17:23:01 +0800

    Create DepartmentStatus.cs
    Add DepartmentStatus.cs Models

---
+++ Models/DepartmentStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BSCS_32E1_HOSPITAL_MANAGEMENT_SYSTEM.Models
+{
+    public class DepartmentStatus
+    {
+        public string Name { get; set; }
+    }
+}

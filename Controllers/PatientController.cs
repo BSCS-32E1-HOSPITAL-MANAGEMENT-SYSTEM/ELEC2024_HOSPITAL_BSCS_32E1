b9a4c1985fc3d90b22e99c086f81c1d7581fa542
Maurice Gabriel Gagarin <160976648+Gagarin-Maurice-BSCS32E1@users.noreply.github.com>
2024-06-19 00:30:03 +0800

    Update PatientController.cs

--- Controllers/PatientController.cs
+++ Controllers/PatientController.cs
@@ -1,12 +1,49 @@
-using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Hospital_Management_System.CollectionViewModels;
+using Hospital_Management_System.Models;
+using Microsoft.AspNet.Identity;
 
-namespace BSCS_32E1_HOSPITAL_MANAGEMENT_SYSTEM.Controllers
+namespace Hospital_Management_System.Controllers
 {
     public class PatientController : Controller
     {
-        public IActionResult Index()
+        private ApplicationDbContext db;
+
+        //Constructor
+        public PatientController()
         {
-            return View();
+            db = new ApplicationDbContext();
         }
-    }
-}
+
+        //Destructor
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+        }
+
+        [Authorize(Roles = "Patient")]
+        public ActionResult Index(string message)
+        {
+            ViewBag.Messege = message;
+            string user = User.Identity.GetUserId();
+            var patient = db.Patients.Single(c => c.ApplicationUserId == user);
+            var date = DateTime.Now.Date;
+            var model = new CollectionOfAll
+            {
+                Ambulances = db.Ambulances.ToList(),
+                Departments = db.Department.ToList(),
+                Doctors = db.Doctors.ToList(),
+                Patients = db.Patients.ToList(),
+                Medicines = db.Medicines.ToList(),
+                ActiveAppointments = db.Appointments.Where(c => c.Status).Where(c => c.PatientId == patient.Id).Where(c => c.AppointmentDate >= date).ToList(),
+                PendingAppointments = db.Appointments.Where(c => c.Status == false).Where(c => c.PatientId == patient.Id).Where(c => c.AppointmentDate >= date).ToList(),
+                AmbulanceDrivers = db.AmbulanceDrivers.ToList(),
+                Announcements = db.Announcements.Where(c => c.AnnouncementFor == "Patient").ToList()
+            };
+            return View(model);
+        }

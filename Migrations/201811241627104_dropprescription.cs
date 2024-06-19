743f5d4a6c6c8d65fe3e7bd0e1fd59f3a0bc14ba
Warren Albino <warrenalbino277@gmail.com>
2024-06-20 01:31:20 +0800

    Add Migration

---
+++ Migrations/201811241627104_dropprescription.cs
@@ -0,0 +1,18 @@
+namespace BSCS_32E1_HOSPITAL_MANAGEMENT_SYSTEM.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class dropprescription : DbMigration
+    {
+        public override void Up()
+        {
+            
+        }
+        
+        public override void Down()
+        {
+            
+        }
+    }
+}

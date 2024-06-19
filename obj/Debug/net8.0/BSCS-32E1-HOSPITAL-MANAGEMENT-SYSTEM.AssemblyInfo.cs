ed2e10a9a8ad7ed73e27d2518d5fe79be7414f83
Peñaflor_Jose_BSCS32E1 <160807078+Penaflor-Jose-BSCS32E1@users.noreply.github.com>
2024-06-19 17:40:12 +0800

    Create Doctor.cs

--- obj/Debug/net8.0/BSCS-32E1-HOSPITAL-MANAGEMENT-SYSTEM.AssemblyInfo.cs
+++ obj/Debug/net8.0/BSCS-32E1-HOSPITAL-MANAGEMENT-SYSTEM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BSCS-32E1-HOSPITAL-MANAGEMENT-SYSTEM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c72e0c6dc74875cb30126cce70141ca05b43aac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29be64fe63a962d86fade493a390dfb237715fe8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BSCS-32E1-HOSPITAL-MANAGEMENT-SYSTEM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BSCS-32E1-HOSPITAL-MANAGEMENT-SYSTEM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

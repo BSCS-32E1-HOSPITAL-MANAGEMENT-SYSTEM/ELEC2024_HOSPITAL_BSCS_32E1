9d036430012e351ee2623a131907012e65f41328
Marlon Christopher Arciaga <161114057+Arciaga-Marlon-BSCS32E1@users.noreply.github.com>
2024-06-20 19:18:14 +0800

    Update IdentityModels.cs

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
-namespace BSCS_32E1_HOSPITAL_MANAGEMENT_SYSTEM.Models
+namespace Hospital_Management_System.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser

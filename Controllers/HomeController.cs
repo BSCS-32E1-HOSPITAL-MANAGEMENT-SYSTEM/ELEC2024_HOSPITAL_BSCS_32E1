9a182f49f68f9805ec3dc2ede7d560f151a34051
Marlon Christopher Arciaga <161114057+Arciaga-Marlon-BSCS32E1@users.noreply.github.com>
2024-06-20 19:16:20 +0800

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,11 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using BSCS_32E1_HOSPITAL_MANAGEMENT_SYSTEM.Models;
+using Hospital_Management_System.Models;
 
-namespace BSCS_32E1_HOSPITAL_MANAGEMENT_SYSTEM.Controllers
+namespace Hospital_Management_System.Controllers
 {
     public class HomeController : Controller
     {
